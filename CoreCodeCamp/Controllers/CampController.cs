67ff682f5dc616712ac6c1b6d8e1f8c72c33ce46
Gregory Sanchez <gsanchez@evopointsolution.com>
2021-10-01 10:42:21 -0400

    Add project files.

---
+++ CoreCodeCamp/Controllers/CampController.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+        public CampController(ICampRepository repository, IMapper mapper)
+        => (_repository, _mapper) = (repository, mapper);
+
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> Get(bool includeTalks = false)
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync(includeTalks);
+
+                CampModel[] models = _mapper.Map<CampModel[]>(results);
+
+                return models;
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Datavase failure");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public ActionResult<CampModel> Get(string moniker)
+        {
+            try
+            {
+                var result = _repository.GetCampAsync(moniker);
+
+                if (result == null)
+                    return NotFound();
+
+                return _mapper.Map<CampModel>(result);     
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Datavase failure");
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<CampModel[]>> SearchByDate(DateTime theDate, bool includeTalks = false)
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsByEventDate(theDate, includeTalks);
+
+                if (!results.Any()) return NotFound();
+
+                return _mapper.Map<CampModel[]>(results);
+
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Datavase failure");
+            }
+        }
+
+    }
+}
---
+++ CoreCodeCamp/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+
+            bool goodStudent = true;
+
+            float average = (goodStudent) ? 85 : 70;
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
